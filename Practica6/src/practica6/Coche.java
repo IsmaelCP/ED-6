faf529e44e28128d14b0b3d13d15c2866c611df4
ismaelcp <exfacia@gmail.com>
2019-06-21 11:19:18 +0200

    Realizar Factorización #4

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -2,69 +2,107 @@ package practica6;
 
 public class Coche {
 	String matricula;
-	String atrib;
+	String combustible;
 	String modelo;
 	String fabricante;
 
+
 	public Coche() {
 		matricula = "";
-		atrib = "";
+		combustible = "";
 		modelo = "";
 		fabricante = "";
 	}
 
 	public Coche(String m, String c, String mo, String f) {
 		matricula = m;
-		atrib = c;
+		combustible = c;
 		modelo = mo;
 		fabricante = f;
 	}
 
-	public String metodo1() {
-		String resultado = "";
-		if (atrib == "Gasolina") {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoA(modelo, fabricante);
-		} else if (atrib == "Diesel") {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoB(modelo, fabricante);
-		} else if (atrib == "Híbrido") {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoC(modelo, fabricante);
-		} else {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += "no dispone de información";
-		}
-		return resultado;
+	public String getMatricula() {
+		return matricula;
 	}
 
-	private String metodoA(String modelo2, String fabricante2) {
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getCombustible() {
+		return combustible;
+	}
+
+	public void setCombustible(String combustible) {
+		this.combustible = combustible;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+
+	static final double precioLitroGasolina = 1.337;
+	static final double precioLitroDiesel = 1.052;
+
+	private String infoGasolina(String modelo2, String fabricante2) {
 		modelo2 = "Corsa";
 		fabricante2 = "Opel";
 		String resultado = "";
 
-		resultado = "es un " + fabricante2 + modelo2 + " y gasta 1,337 euros por litro"; 
+		resultado = "es un " + fabricante2 + modelo2 + " y gasta " + precioLitroGasolina + " euros por litro"; 
 
 		return resultado;
 	}
 
-	private String metodoB(String modelo2, String fabricante2) {
+	private String infoDiesel(String modelo2, String fabricante2) {
 		modelo2 = "Ibiza";
 		fabricante2 = "Seat";
 		String resultado = "";
 
-		resultado = "es un " + fabricante2 + modelo2 + " y gasta 1,337 euros por litro"; 
+		resultado = "es un " + fabricante2 + modelo2 + " y gasta " + precioLitroDiesel + " euros por litro"; 
 
 		return resultado;
 	}
 
-	private String metodoC(String modelo2, String fabricante2) {
+	private String infoHibrido(String modelo2, String fabricante2) {
 		modelo2 = "Getz";
 		fabricante2 = "Hyundai";
 		String resultado = "";
 
-		resultado = "es un " + fabricante2 + modelo2 + " y gasta 1,337 euros por litro"; 
+		resultado = "es un " + fabricante2 + modelo2 + " y no necesita combustible"; 
 
+		return resultado;
+	}
+	
+	public String infoCoche() {
+		String resultado = "";
+		String texto = "El coche con matricula ";
+		if (combustible == "Gasolina") {
+			resultado += texto + matricula + " ";
+			resultado += infoGasolina(modelo, fabricante);
+		} else if (combustible == "Diesel") {
+			resultado += texto + matricula + " ";
+			resultado += infoDiesel(modelo, fabricante);
+		} else if (combustible == "Híbrido") {
+			resultado += texto + matricula + " ";
+			resultado += infoHibrido(modelo, fabricante);
+		} else {
+			resultado += texto + matricula + " ";
+			resultado += "no dispone de información";
+		}
 		return resultado;
 	}
 
--- Practica6/src/practica6/Moto.java
+++ Practica6/src/practica6/Moto.java
@@ -1,17 +1,17 @@
 package practica6;
 
 public class Moto {
-	String matricula;
-	int cilindrada;
+	private String matricula;
+	private int cilindrada;
 
 	public Moto() {
-		matricula = "";
-		cilindrada = 0;
+		setMatricula("");
+		setCilindrada(0);
 	}
 
 	public Moto(String m, int c) {
-		matricula = m;
-		cilindrada = c;
+		setMatricula(m);
+		setCilindrada(c);
 	}
 
 	public String getMatricula() {
